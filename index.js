5995302d7c2e84eaa112e18cd20439ff7ac0b003
waaazif <wazifirfan1210@gmail.com>
2022-02-04 19:56:18 +0800

    FIRST COMMIT

---
+++ index.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".home-slider",{
+    spaceBetween: 20,
+    effect: "fade",
+    grabCursor: true,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+      },
+  });
